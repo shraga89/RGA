96da755077ee3ffcdb4bac98d124c96e71bea3f5
gregory goren <grggoren@gmail.com>
2020-02-13 11:04:18 +0200

    Added cool functionality to visualization...cool AF!

--- Visualization.py
+++ Visualization.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 
 class Visualization:
     def __init__(self,history):
@@ -7,6 +8,13 @@ class Visualization:
 
     #TODO: add functionality to different kinds of figures - will be added after we know wtf we want
     def create_figure(self, x, y_axes, y_label, x_label, legends, colors, output_fname):
+
+        if not os.path.exists(os.path.dirname(output_fname)):
+            try:
+                os.makedirs(os.path.dirname(output_fname))
+            except:
+                output_fname=os.path.basename(output_fname)
+
         params = {'legend.fontsize': 'x-large',
                   'figure.figsize': (13, 8),
                   'axes.labelsize': 'x-large',
@@ -41,7 +49,16 @@ class Visualization:
         else:
             raise ValueError("undefined statistic parameter for plot")
 
-    def create_price_plot(self,stat="mean"):
+
+    def get_df_stats_and_create_plot(self,df,stat="mean",product = None):
+        buyer_prices = self.infer_aggregated_stats(df, "buying_price", stat)
+        seller_prices = self.infer_aggregated_stats(df, "selling_price", stat)
+        y_axes = [buyer_prices, seller_prices]
+        x = df["turn"].drop_duplicates().values
+        fname_addition = product+"_" if product is not None else ""
+        self.create_figure(x, y_axes, stat + " price", "iteration", ["Buyers", "Sellers"], ["b", "r"], "plt/"+fname_addition+stat + "_prices")
+
+    def create_price_over_all_products_plot(self, stat="mean"):
         """
         Creates plot for price statistics while considering only succesfull transactions.
 
@@ -50,8 +67,12 @@ class Visualization:
         """
         df = self.history.copy()
         df = df[df['outcome'] == 'successful']
-        buyer_prices = self.infer_aggregated_stats(df,"buying_price",stat)
-        seller_prices = self.infer_aggregated_stats(df,"selling_price",stat)
-        y_axes = [buyer_prices,seller_prices]
-        x = df["turn"].drop_duplicates().values
-        self.create_figure(x,y_axes,stat+" price","iteration",["Buyers","Sellers"],["b","r"],stat+"_prices")
+        self.get_df_stats_and_create_plot(df,stat)
+
+    def create_price_per_product_plot(self,stat="mean"):
+        df = self.history.copy()
+        df = df[df['outcome'] == 'successful']
+        products = df["product"].drop_duplicates().values
+        for product in products:
+            product_projected_history = df[df["product"]==product]
+            self.get_df_stats_and_create_plot(product_projected_history,stat,product)
--- tests/visualization_test.py
+++ tests/visualization_test.py
@@ -3,8 +3,18 @@ import pandas as pd
 
 df  = pd.read_csv("../sim.csv")
 visualizer = Visualization(df)
-visualizer.create_price_plot("mean")
-visualizer.create_price_plot("median")
-visualizer.create_price_plot("min")
-visualizer.create_price_plot("max")
-visualizer.create_price_plot("kaka")
+visualizer.create_price_over_all_products_plot("mean")
+visualizer.create_price_over_all_products_plot("median")
+visualizer.create_price_over_all_products_plot("min")
+visualizer.create_price_over_all_products_plot("max")
+
+try:
+    visualizer.create_price_over_all_products_plot("kaka")
+except ValueError as e:
+    print(str(e))
+
+
+visualizer.create_price_per_product_plot("mean")
+visualizer.create_price_per_product_plot("max")
+visualizer.create_price_per_product_plot("min")
+visualizer.create_price_per_product_plot("median")
