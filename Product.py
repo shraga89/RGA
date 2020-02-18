4b867c48e2397cfbf21f15d48659e778c92d244f
gregory goren <grggoren@gmail.com>
2020-02-18 14:51:14 +0200

    Product bitch!

---
+++ Product.py
@@ -0,0 +1,16 @@
+class Product:
+    def __init__(self, name):
+        self.name = name
+
+
+class DataProduct(Product):
+
+    def __init__(self, name, domain, number_of_examples, number_of_features, additional_features):
+        super(DataProduct, self).__init__(name)
+        self.domain = domain
+        self.number_of_examples = number_of_examples
+        self.number_of_features = number_of_features
+        self.additional_features = additional_features
+
+    def __eq__(self, other):
+        return self.name == other.name and self.domain == other.domain
