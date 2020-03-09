98d52fc3436ed2f13a83b09da812f718eae486aa
alex <queldelan@gmail.com>
2020-03-09 18:42:22 +0200

    First and second price auction = done as fuck

--- Contract.py
+++ Contract.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+import operator
 
 
 class Contract:
@@ -37,3 +38,71 @@ class SimpleSellingContract(Contract):
         seller.budget += actual_price
         buyer.budget -= actual_price
         return actual_price
+
+
+class Auction:
+
+    def __init__(self):
+        pass
+
+    def run_auction(self, bids, **kwargs):
+        winner = self.winner_determination(bids, **kwargs)
+        prices = self.price_determination(bids, **kwargs)
+        return winner, prices
+
+    @abstractmethod
+    def winner_determination(self, bids, **kwargs):
+        pass
+
+    @abstractmethod
+    def price_determination(self, bids, **kwargs):
+        pass
+
+
+class FirstPriceAuction(Auction):
+
+    def __init__(self):
+        super().__init__()
+
+    def winner_determination(self, bids, **kwargs):
+        return max(bids.items(), key=operator.itemgetter(1))[0]
+
+    def price_determination(self, bids, **kwargs):
+        return max(bids.items(), key=operator.itemgetter(1))[1]
+
+
+class SecondPriceAuction(Auction):
+
+    def __init__(self):
+        super().__init__()
+
+    def winner_determination(self, bids, **kwargs):
+        if not bids:
+            return None
+        threshold_price = kwargs.get("threshold_price", 0)
+        winner = max(bids.items(), key=operator.itemgetter(1))[0]
+        if bids[winner] >= threshold_price:
+            return winner
+        else:
+            return None
+
+    def price_determination(self, bids, **kwargs):
+        winner = self.winner_determination(bids, **kwargs)
+        if winner is None:
+            return None
+        threshold_price = kwargs.get("threshold_price", 0)
+        if len(bids) == 1:
+            return threshold_price
+        second_place_player = sorted(bids.keys(), key=lambda x: (bids[x], x), reverse=True)[1]
+        price_candidate = bids[second_place_player]
+        if price_candidate >= threshold_price:
+            return price_candidate
+        else:
+            return threshold_price
+
+
+if __name__=="__main__":
+    my_auction = SecondPriceAuction()
+    # bids = {"greg":3, "alex":2,'shraga':1}
+    bids = {}
+    print(my_auction.run_auction(bids, threshold_price = 1.5))
